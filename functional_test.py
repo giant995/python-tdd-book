ad86ba517df31b6990eb7781090b7982965d3d2d
giant995 <jmichel.plourde@gmail.com>
2019-05-09 11:58:36 -0400

    Run checks for a second item with automatic list numbers

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # John Doe has heard about a cool new online to-do app. He
         # goes to check out its homepage
@@ -31,16 +36,13 @@ class NewVisitorTest(unittest.TestCase):
         # He types "Buy peacock feathers" into a text box
         inputbox.send_keys('Buy peacock feathers')
 
-        # Whe he hits enter, the page updates, and now the page lists
+        # When he hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
-        # There is still a text box inviting her to add another item. She
+        # There is still a text box inviting him to add another item. He 
         # enters "Use peacock feathers to make a fly" (John is very
         # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -49,13 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         # The page updates again, and now shows both items on his list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # John wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
